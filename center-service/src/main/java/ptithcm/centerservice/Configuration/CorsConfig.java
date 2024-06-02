03c70b88c10612fe600781e582300cb407befc46
Rangdog <92283489+Rangdog@users.noreply.github.com>
2024-06-03 01:31:30 +0700

    sửa cors

---
+++ center-service/src/main/java/ptithcm/centerservice/Configuration/CorsConfig.java
@@ -0,0 +1,17 @@
+package ptithcm.centerservice.Configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173") // Thay đổi domain của bạn tại đây
+                .allowedMethods("GET") // Các phương thức HTTP được phép
+                .allowCredentials(true) // Cho phép gửi cookie qua CORS
+                .maxAge(3600); // Cache cấu hình CORS trong 1 giờ
+    }
+}
