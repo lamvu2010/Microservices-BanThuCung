d34c3c32e2e850b62204c432c2c3cebedb0ceba0
Rangdog <92283489+Rangdog@users.noreply.github.com>
2024-06-10 11:53:36 +0700

    sửa lại ko đc tenChiNhanh.replace("\"", "");

--- center-service/src/main/java/ptithcm/centerservice/Controller/ChiNhanhController.java
+++ center-service/src/main/java/ptithcm/centerservice/Controller/ChiNhanhController.java
@@ -57,7 +57,6 @@ public class ChiNhanhController {
 
     @PostMapping
     public ResponseEntity<?> insert(@RequestBody String tenChiNhanh) {
-        tenChiNhanh = tenChiNhanh.replace("\"", "");
         if (chiNhanhService.existsByTenChiNhanh(tenChiNhanh)) {
             return new ResponseEntity<>("Entity is existed", HttpStatus.CONFLICT);
         }
